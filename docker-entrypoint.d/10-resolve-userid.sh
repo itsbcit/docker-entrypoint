569aa608cb34ef16db7dc219a776c2ee60b95d59
Jesse Weisner <jesse_weisner@bcit.ca>
2019-03-06 12:25:32 -0800

    Feedback if auth files are not writable

--- docker-entrypoint.d/10-resolve-userid.sh
+++ docker-entrypoint.d/10-resolve-userid.sh
@@ -3,6 +3,13 @@
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
     echo "${RUNUSER}:x:`id -u`:0:OpenShift ${RUNUSER}:$HOME:/bin/bash" >> /etc/passwd
+  else
+    echo "Failed to update /etc/passwd. Not writable." >&2
+  fi
+
+  if [ -w /etc/group ]; then
     echo "${RUNUSER}:x:`id -u`:${RUNUSER}" >> /etc/group
+  else
+    echo "Failed to update /etc/group. Not writable." >&2
   fi
 fi
